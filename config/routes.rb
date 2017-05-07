6cea5a844817710f9b3f7211c822e027fe332a7d
Stephen Grice <stephengrice@live.com>
2017-05-07 14:36:41 -0400

    Finish basic template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  
+  get '/login' => 'sessions#new', name: 'login_path'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/register' => 'users#new', name: 'register_path'
+  post '/users' => 'users#create'
 end
